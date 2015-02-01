0e7e974586896ea0b1da4cb4eecfc28849befe4e
Joseph Azofeifa <azofeifa@vieques.colorado.edu>
2015-01-31 19:41:56 -0700

    update qsub vieques

--- qsub_script_vieques.sh
+++ qsub_script_vieques.sh
@@ -1,11 +1,8 @@
 #PBS -N IE_FIT
 #PBS -l nodes=1:ppn=32
-<<<<<<< HEAD
 #PBS -e /Users/azofeifa/qsub_errors/
 #PBS -o /Users/azofeifa/qsub_stdo/
-=======
-#PBS -t 1-100%10
->>>>>>> 96370ee33a76e48691c035e4f75fa950a953a0ec
+#PBS -t 1-500%20
 module load numpy_1.6.1
 module load scipy_0.12.0
 module load python_2.7.3
@@ -21,10 +18,11 @@ strand=+
 chrom=chr1
 single=1
 test=0
-rt=5
-BIC_MAX=5
+rt=10
+BIC_MAX=10
 BIC_PEN=$PBS_ARRAYID
-outFile=${chrom}_${BIC_MAX}_${BIC_PEN}_${rt}_IE_OUT.bed
+BIN=500
+outFile=${chrom}_${BIC_MAX}_${BIC_PEN}_${rt}_${BIN}_IE_OUT.bed
 np=32
 if [ $test == "1" ]
 then
@@ -32,7 +30,7 @@ then
 else
     if [ $single == "1" ]
     then
-	python $pathTosrc classify -i ${inFilePath}$BG_FILE -j ${inFilePath}$annot_FILE -o ${outFilePath}$outFile -s $strand -chr $chrom  -BIC $BIC_MAX $BIC_PEN -single -np $np -rt $rt
+	python $pathTosrc classify -i ${inFilePath}$BG_FILE -j ${inFilePath}$annot_FILE -o ${outFilePath}$outFile -s $strand -chr $chrom  -BIC $BIC_MAX $BIC_PEN -single -np $np -rt $rt -bin $BIN
     else
 	python $pathTosrc classify -i ${inFilePath}$BG_FILE -j ${inFilePath}$annot_FILE -o ${outFilePath}$outFile -s $strand -chr $chrom  -BIC $BIC_MAX $BIC_PEN -np $np -rt $rt -merge
     fi
