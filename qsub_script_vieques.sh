abe95e6ccf9df01b90cdfb6be85c8f8c20041d52
Joseph Azofeifa <azofeifa@vieques.colorado.edu>
2015-02-02 12:37:00 -0700

    update

--- qsub_script_vieques.sh
+++ qsub_script_vieques.sh
@@ -2,7 +2,7 @@
 #PBS -l nodes=1:ppn=32
 #PBS -e /Users/azofeifa/qsub_errors/
 #PBS -o /Users/azofeifa/qsub_stdo/
-#PBS -t 0-20
+#PBS -t 0-50
 module load numpy_1.6.1
 module load scipy_0.12.0
 module load python_2.7.3
@@ -13,10 +13,10 @@ inFilePath=${root}inFiles_IE_FIT/
 pathTosrc=${root}IE_FIT/NU_FIT/
 
 BG_FILE=DMSO2_3.sorted.fiveprime.pos.BedGraph
-annot_FILE=RefSeq.NU.tsv
-strand=+
+annot_FILE=FStitch_neg_Annotations.tsv
+strand=-
 chrom=chr1
-single=1
+single=0
 test=0
 rt=10
 BIC_MAX=10
@@ -34,7 +34,7 @@ else
     then
 	python $pathTosrc classify -i ${inFilePath}$BG_FILE -j ${inFilePath}$annot_FILE -o ${outFilePath}$outFile -s $strand -chr $chrom  -BIC $BIC_MAX $BIC_PEN -single -np $np -rt $rt -bin $BIN
     else
-	python $pathTosrc classify -i ${inFilePath}$BG_FILE -j ${inFilePath}$annot_FILE -o ${outFilePath}$outFile -s $strand -chr $chrom  -BIC $BIC_MAX $BIC_PEN -np $np -rt $rt -merge
+	python $pathTosrc classify -i ${inFilePath}$BG_FILE -j ${inFilePath}$annot_FILE -o ${outFilePath}$outFile -s $strand -chr $chrom  -BIC $BIC_MAX $BIC_PEN -np $np -rt $rt -bin $BIN -int $int
     fi
 fi
 
