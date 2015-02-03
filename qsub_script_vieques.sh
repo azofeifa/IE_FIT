167d87f92f8611c8fb09b3c96c3bac6cf77db729
Joseph Azofeifa <azofeifa@vieques.colorado.edu>
2015-02-03 12:31:28 -0700

    updated qsub

--- qsub_script_vieques.sh
+++ qsub_script_vieques.sh
@@ -4,6 +4,8 @@
 #PBS -o /Users/azofeifa/qsub_stdo/
 #PBS -t 0-50
 #PBS -l walltime=05:00:00
+#PBS -m ae
+#PBS -M jgazofeifa@gmail.com
 module load numpy_1.6.1
 module load scipy_0.12.0
 module load python_2.7.3
@@ -14,19 +16,20 @@ inFilePath=${root}inFiles_IE_FIT/
 pathTosrc=${root}IE_FIT/NU_FIT/
 
 BG_FILE=DMSO2_3.sorted.fiveprime.pos.BedGraph
-annot_FILE=FStitch_pos_Annotations.tsv
+annot_FILE=refGene_convert.tsv
 strand=+
 chrom=chr1
 single=0
 test=0
-rt=10
-BIC_MAX=10
+rt=15
+BIC_MAX=5
 BIC_PEN=15
-BIN=1000
+BIN=10000
 int=$PBS_ARRAYID
 outFile=${chrom}_${BIC_MAX}_${BIC_PEN}_${rt}_${BIN}_${strand}_${int}_IE_OUT.bed
 np=32
-
+pad1=200
+pad2=1000
 if [ $test == "1" ]
 then
     python $pathTosrc classify -i ${inFilePath}$BG_FILE -j ${inFilePath}$annot_FILE -o ${outFilePath}$outFile -s $strand -chr $chrom -v -BIC $BIC_MAX $BIC_PEN -np $np -t
@@ -35,7 +38,7 @@ else
     then
 	python $pathTosrc classify -i ${inFilePath}$BG_FILE -j ${inFilePath}$annot_FILE -o ${outFilePath}$outFile -s $strand -chr $chrom  -BIC $BIC_MAX $BIC_PEN -single -np $np -rt $rt -bin $BIN
     else
-	python $pathTosrc classify -i ${inFilePath}$BG_FILE -j ${inFilePath}$annot_FILE -o ${outFilePath}$outFile -s $strand -chr $chrom  -BIC $BIC_MAX $BIC_PEN -np $np -rt $rt -bin $BIN -int $int
+	python $pathTosrc classify -i ${inFilePath}$BG_FILE -j ${inFilePath}$annot_FILE -o ${outFilePath}$outFile -s $strand -chr $chrom  -BIC $BIC_MAX $BIC_PEN -np $np -rt $rt -bin $BIN -int $int -pad $pad1 $pad2 -merge
     fi
 fi
 
